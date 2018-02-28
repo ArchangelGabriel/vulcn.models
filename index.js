eb213c42a959e72aa36dd66fbe2823ac990de743
Gabe Trinidadd <gabe.trinidad@vulcansearch.com>
2018-02-28 11:21:47 -0600

    Connects to mongodb immediately based on different MONGO_URL_NODE_ENV

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-module.exports = {
-  Account: require('./entities/Account'),
-  Authorization: require('./entities/Authorization'),
-  Company: require('./entities/Company'),
-  User: require('./entities/User'),
-}
+const mongoose = require('mongoose')
+const entities = require('./entities')
+
+const NODE_ENV = process.env.NODE_ENV && process.env.NODE_ENV.toUpperCase()
+const CONNECTION_STRING = process.env[`MONGO_URL_${NODE_ENV}`]
+if (CONNECTION_STRING) mongoose.connect(CONNECTION_STRING)
+
+module.exports = entities
