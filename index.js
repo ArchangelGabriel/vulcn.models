c7878eb244875e0bdb334e13b2521031fd1f87e0
Gabe Trinidadd <gabe.trinidad@vulcansearch.com>
2018-03-01 16:56:56 -0600

    Added keepAlive and autoIndex opts

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const entities = require('./entities')
 
 const NODE_ENV = process.env.NODE_ENV && process.env.NODE_ENV.toUpperCase()
 const CONNECTION_STRING = process.env[`MONGO_URL_${NODE_ENV}`]
-if (CONNECTION_STRING) mongoose.connect(CONNECTION_STRING)
+const isProd = NODE_ENV === 'production'
+
+if (CONNECTION_STRING) mongoose.connect(CONNECTION_STRING, {
+  autoIndex: isProd ? false : true,
+  keepAlive: isProd ? false : true,
+})
 
 module.exports = entities
