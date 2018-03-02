bf2e1f48860cca35076e1db33d5ba2a500d8be7a
mikemahon <mike.mahon@vulcansearch.com>
2018-03-02 11:35:04 -0600

    adds Authorizations discriminator

--- entities/Authorization.js
+++ entities/Authorization.js
@@ -7,8 +7,27 @@ const schema = new mongoose.Schema({
   accessToken: String,
   refreshToken: String,
   scopes: [String],
+  platform: {type: String, enum: ['Google', 'Bing']}
 }, {
   timestamps: true,
+  discriminatorKey: 'platform'
 })
+
+var Authorization = mongoose.model('Authorization', schema)
+
+var Google = Authorization.discriminator('Google', new mongoose.Schema({
+  sub: { type: String, required: true },
+  token_expiry: { type: Date },
+  client_id: { type: String }
+}))
+
+var Bing = Authorization.discriminator('Bing', new mongoose.Schema({
+  user_id: { type: String, required: true },
+  expires_in: { type: Date },
+}))
+
+
+
+
 
 module.exports = mongoose.model('Authorization', schema)
