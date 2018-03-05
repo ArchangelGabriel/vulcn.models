87220940f632ef3b8b5c7da44d58650deb826226
Gabe Trinidadd <gabe.trinidad@vulcansearch.com>
2018-03-05 16:35:57 -0600

    Added more unit tests

--- test/User.test.js
+++ test/User.test.js
@@ -1,3 +1,5 @@
+process.env.SECRET_TEST = 'secret_string'
+
 const { User } = require('../entities')
 const hashPassword = require('../modelhooks/hashPassword')
 
@@ -34,4 +36,28 @@ test('should be able to identify incorrect password', (done) => {
       done()
     })
   })
+})  
+
+test('should generate jwt', (done) => {
+  const JWT_REGEX = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/
+  const email = 'test_email@vulcn.ai'
+  const password = 'test_password'
+
+  let u = new User({ email, password })
+  let jwt = u.generateJWT()
+  expect(jwt).toBeDefined()
+  expect(JWT_REGEX.test(jwt)).toEqual(true)
+  done()
+})
+
+test('toAuthJSON should contain _id, email and token', (done) => {
+  const email = 'test_email@vulcn.ai'
+  const password = 'test_password'
+
+  let u = new User({ email, password })
+  let authObj = u.toAuthJSON()
+
+  const attributes = ['_id', 'email', 'token']
+  attributes.forEach((attr) => expect(authObj[attr]).toBeDefined())
+  done()
 })
