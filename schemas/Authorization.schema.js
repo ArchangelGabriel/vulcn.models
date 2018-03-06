5daded7ab84add4a1f1c0c59636e8d25e6af4d5f
Gabe Trinidadd <gabe.trinidad@vulcansearch.com>
2018-03-06 15:38:06 -0600

    Updated authorization gql schema

--- schemas/Authorization.schema.js
+++ schemas/Authorization.schema.js
@@ -1,15 +1,25 @@
 module.exports = `
+enum PlatformType {
+  Google
+  Bing
+}
+
 type Authorization {
   _id: ID
   account: Account
   email: String
   accessToken: String
+  refreshToken: String
   scopes: [String]
+  platform: PlatformType
 }
 
 input AuthorizationInput {
+  account: AccountInput
   email: String
   accessToken: String
-  scope: [String]
+  refreshToken: String
+  scopes: [String]
+  platform: PlatformType
 }
 `
